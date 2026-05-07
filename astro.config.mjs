1160258f42e1ab1d73bee5979b5bc3922f16c0ed
Jeremy Bise <jeremy.bise@gmail.com>
2026-05-07 12:16:23 -0400

    feat: add Netlify adapter and remove dynamic post page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import netlify from "@astrojs/netlify";
+import keystatic from "@keystatic/astro";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
+  adapter: netlify(),
 
   vite: {
     plugins: [tailwindcss()],
